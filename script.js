8620821b36fc28f08366a60844d67f5daa97676f
Doni.Khamrayev <doniyr.khamrayev@mail.ru>
2025-02-06 14:46:42 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 ////////////////////////task1////////////////////////
-let calc = (prompt("Введите число"));
+let calc = prompt("Введите число");
 for (let i = 1; i <= 10; i++) {
     console.log(`${calc} * ${i} = ${calc * i}`); 
 }
